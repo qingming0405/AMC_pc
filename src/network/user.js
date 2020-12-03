f2b3b62a4f41442ec790614ca72f0298d3441bcd
tianhe <595417564@qq.com>
2020-12-03 15:21:07 +0800

    Enter

--- src/network/user.js
+++ src/network/user.js
@@ -5,3 +5,21 @@ export function getAllUserInfo() {
     url: '/getAllUserInfo'
   })
 }
+
+export function insertAndUpdateUsers(userInfos) {
+  return request({
+    url: '/insertAndUpdateUsers',
+    params: {
+      userInfos
+    }
+  })
+}
+
+export function delUserById(delIds) {
+  return request({
+    url: '/delUserById',
+    params:{
+      delIds
+    }
+  })
+}
